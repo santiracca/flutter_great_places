10e12c0daf79ec546dc1593683b019478658f5fa
santiracca <santiagoracca7@gmail.com>
2020-04-28 13:54:17 -0400

    Add basic app flow

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+import '../models/place.dart';
+import 'dart:io';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: image,
+      title: title,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+  }
+}
---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
+import 'dart:io';
+import 'package:provider/provider.dart';
+import '../providers/great_places.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = new TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      _pickedImage,
+    );
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a new place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            color: Theme.of(context).accentColor,
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            icon: Icon(Icons.add),
+            label: Text('Add place'),
+            onPressed: _savePlace,
+          ),
+        ],
+      ),
+    );
+  }
+}
